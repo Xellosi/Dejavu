6aa0ed127c638a0f312cd2e53c3fcdb923919a3c
陳凱勛 <larry110864@gmail.com>
2018-04-22 15:16:53 +0800

    no message

---
+++ Assets/Script/UGUI_move_final.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UGUI_move_final :MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+    [SerializeField] GameObject grid = null;
+    [SerializeField] GameObject initCanvas = null;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        GetComponent<RectTransform>().pivot.Set(0, 0);
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+        transform.SetParent(initCanvas.transform, true);
+        transform.GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(1f, 1f, 1f);
+        if (eventData.pointerCurrentRaycast.gameObject != null)
+        {
+            if (eventData.pointerCurrentRaycast.gameObject.name == grid.name)
+            {
+                transform.SetParent(grid.transform);
+            }
+        }
+        transform.GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+}
+
