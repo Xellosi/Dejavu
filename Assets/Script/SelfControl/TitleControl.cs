74eb0349a628ae066e884b8d4f384afdae624686
Xellosi <gsgs895552@gmail.com>
2018-07-15 20:31:33 +0800

    no message

---
+++ Assets/Script/SelfControl/TitleControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TitleControl : MonoBehaviour {
+	Button b;
+	// Use this for initialization
+	void Start () {
+		b = GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void ClosePanel(){
+		transform.parent.gameObject.SetActive (false);
+	}
+
+	public void OpenPanel(GameObject a){
+		a.SetActive (true);
+	}
+}
